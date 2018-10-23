d5249ac83f99b7e990616296fce736d010a252d9
Daniel JAHENY <44342309+F-ox@users.noreply.github.com>
2018-10-23 14:22:26 +0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -3,13 +3,6 @@ def start_game():
   print("--------------------------------------\n Welcome to the Number Guessing Game! \n --------------------------------------")
 
 
-  try:
-    name= input("What's your name? ")
-    print("Hi {}, my name is HAL 9000 *beep* *boop*. \n I am thinking of a number between 1 and 10, can you guess which number it is? *beep* *boop*? \n...Are your ready?".format(name))
-  except NameError:
-    print("Oops, this is not a correct name:)")
-    name= input("What's your name? ")
-
   import random
   import sys
   from random import randrange
@@ -17,6 +10,13 @@ def start_game():
   attempts = 1
 
   answer = randrange(1,10)
+  
+  try:
+    name= input("What's your name? ")
+    print("Hi {}, my name is HAL 9000 *beep* *boop*. \n I am thinking of a number between 1 and 10, can you guess which number it is? *beep* *boop*? \n...Are your ready?".format(name))
+  except NameError:
+    print("Oops, this is not a correct name:)")
+    name= input("What's your name? ")
 
   try:
     guess = int(input("...What is the number? "))
